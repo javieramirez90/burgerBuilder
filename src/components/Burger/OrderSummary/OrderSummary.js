f5903cee5247b8bc586569c3a006bcfce35301ea
javieramirez90 <javier.ramirez@aiesec.net>
2019-01-11 16:31:47 -0600

    Modal, not finished

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>A delicious burguer with the following ingredients: </p>
+      <ul>
+        
+      </ul>
+    </Aux>
+  ); 
+}
+
+export default orderSummary
