9d9ee98e8688ad5dbd53a4e00123933b8269a391
javieramirez90 <javier.ramirez@aiesec.net>
2019-01-11 16:59:29 -0600

    Modal no dynamic content

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,13 +3,22 @@ import React from 'react';
 import Aux from '../../../hoc/Aux';
 
 const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return (
+        <li>
+          <span style={{textTransform: "capitalize"}}>{ igKey }</span>: {props.ingredients[igKey]}
+        </li>);
+    });
+
   return (
     <Aux>
       <h3>Your order</h3>
       <p>A delicious burguer with the following ingredients: </p>
       <ul>
-        
+        {ingredientSummary}
       </ul>
+      <p>Continue to Checkout?</p>
     </Aux>
   ); 
 }
