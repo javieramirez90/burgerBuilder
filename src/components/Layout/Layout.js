0e7e92fee5254b838399ee4df9eb8fdb914d167d
javieramirez90 <javier.ramirez@aiesec.net>
2019-01-12 23:52:41 -0600

    Modal backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,6 +5,7 @@ import classes from './Layout.css'
 
 const layout = (props) => (
   <Aux>
+    
     <div>Toolbar, SideDrawer, BackDrop</div>
     <main className={classes.Content}>
       {props.children}
